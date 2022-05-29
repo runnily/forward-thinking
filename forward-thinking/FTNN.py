3d0574daca41c24143c9f349bae814fc773f47ff
Adanna <aaddanna@outlook.com>
2022-05-29 23:58:35 +0000

    added a measure class for measuring the accuracy

--- forward-thinking/FTNN.py
+++ forward-thinking/FTNN.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import datasets
 import paras
+from utils import Measure
 import layers
 from torch import device, cuda
 DEVICE = device('cuda' if cuda.is_available()  else 'cpu')
@@ -19,6 +20,7 @@ class FTNN(nn.Module):
       self.additional_layers = layers # layers to be added into our model one at a time
       self.layers = nn.ModuleList([])
       self.frozen_layers = nn.ModuleList([])
+      self.last_layers = nn.ModuleList([self.h0])
       self.classes = classes
 
   def forward(self, x):
@@ -27,13 +29,13 @@ class FTNN(nn.Module):
       x = l(x)
     
 
-    #x = F.max_pool2d(x, kernel_size=x.size()[2:]) 
-    #x = F.dropout2d(x, 0.1, training=True)
+    x = F.max_pool2d(x, kernel_size=x.size()[2:]) 
+    x = F.dropout2d(x, 0.1, training=True)
 
     x = x.reshape(x.shape[0], -1) # flatten to go into the linear hidden layer
 
     x = self.h0(x)
-    x = self.classifer(x)
+    #x = self.classifer(x)
     return x
 
 class Net(nn.Module):
@@ -64,20 +66,25 @@ class Train():
     self.lr = lr
     self.num_epochs = num_epochs
     self.backpropgate = backpropgate
+    self.recordAccuracy = Measure()
 
 
   def optimizer_(self, parameters_to_be_optimized):
     return torch.optim.SGD(parameters_to_be_optimized, lr=self.lr, momentum=0.9)
     #return torch.optim.Adam(parameters_to_be_optimized, lr=self.lr, rho=0.9, eps=1e-3, weight_decay=0.001)
-  
-  def train_(self):
 
-    specific_params_to_be_optimized = []
+  def __accuracy(self, predictions, labels):
+    classes = torch.argmax(predictions, dim=1)
+    return torch.mean((classes == labels).float()) # needs mean for match size
+  
+  def __train(self, specific_params_to_be_optimized):
+
+  
     if self.backpropgate == True:
       specific_params_to_be_optimized = self.model.parameters()
     else: 
       specific_params_to_be_optimized = [{'params': self.model.layers[-1].parameters()},
-                                {'params': self.model.classifer.parameters()}, {'params': self.model.h1.parameters()}]
+                                {'params': self.model.classifer.parameters()}, {'params': self.model.h0.parameters()}]
     
     n_total_steps = len(self.train_loader)
                                 
@@ -92,8 +99,11 @@ class Train():
 
     for epoch in range(self.num_epochs):  # loop over the dataset multiple times
 
-      running_loss = 0.0
-      for i, data in enumerate(self.train_loader, 0):
+      running_loss = 0.00
+      running_accuracy = 0.00
+      start = torch.cuda.Event(enable_timing=True)
+      start.record()
+      for i, data in enumerate(self.train_loader, 0): # looping over every batch
           # get the inputs; data is a list of [inputs, labels]
           inputs, labels = data
           inputs = inputs.to(DEVICE)
@@ -108,14 +118,25 @@ class Train():
           loss.backward()
           optimizer.step()
 
+          running_accuracy += self.__accuracy(outputs, labels)
           # print statistics
           running_loss += loss.item()
           if (i+1) % 100 == 0:
             print("Epoch [{}/{}], Step [{}/{}], Loss: {:.2f}".format(epoch+1,self.num_epochs,i+1,n_total_steps,loss.item()))
-    
-    self.model.eval()
+      end = torch.cuda.Event(enable_timing=True)
+      end.record()
+
+      torch.cuda.synchronize()
+
+      test_accuracy = self.__test()
+      len_self_loader = len(self.train_loader)
+      running_accuracy /= len_self_loader
+      running_loss /= len_self_loader
+      self.recordAccuracy(start.elapsed_time(end), epoch, running_loss, test_accuracy, running_accuracy)
+
         
-  def test_(self):
+  def __test(self):
+    self.model.eval()
     with torch.no_grad():
       n_correct = 0
       n_samples = 0
@@ -129,8 +150,9 @@ class Train():
         n_samples += labels.shape[0] # number of samples in current batch
         n_correct += (predicted == labels).sum().item() # gets the number of correct
 
-    accuracy = (n_correct / n_samples) * 100
-    print("Accuracy is now: {}".format(accuracy))
+    accuracy = n_correct / n_samples
+    self.model.train()
+    return accuracy
 
   def freeze_layers_(self):
     if self.backpropgate == False:
@@ -141,8 +163,7 @@ class Train():
 
     if self.backpropgate == True:
       self.model.layers = self.model.additional_layers
-      self.train_()
-      self.test_()
+      self.__train()
     
     else:
       N = len(self.model.additional_layers)
@@ -153,21 +174,25 @@ class Train():
         self.model.layers.append(layer)
         # 2. diregarded output as output layer is retrained with every new added layer
         self.model.h0 = nn.LazyLinear(out_features=self.model.hidden_layer_features).to(DEVICE)
-        # 3. Train
-        self.train_()
-        # 4. Get Accuracy
-        self.test_()
+        # 3. defining parameters to be optimized
+        specific_params_to_be_optimized = [{'params': self.model.layers[-1].parameters()},
+                                {'params': self.model.classifer.parameters()}, {'params': self.model.h0.parameters()}]
+        # 4. Train 
+        self.__train(specific_params_to_be_optimized)
         # 5. As we have trained add layer to the frozen_layers
         self.model.frozen_layers.append(self.model.layers[-1])
         # 6. Freeze layers
-        self.freeze_layers_()  
+        self.freeze_layers_() 
+        print(self.recordAccuracy)
+        break 
       
-
+    # This part is to train the last layers
     if len(self.model.additional_layers) < 0:
-      pass
+      for i in range(len(self.model.last_layers)):
+        self.__train(self.model.last_layers.parameters())
 
 train_loader, test_loader = datasets.CIFAR_10()
-train = Train(train_loader, test_loader, backpropgate = True)
+train = Train(train_loader, test_loader)
 train.add_layers()
 
     
