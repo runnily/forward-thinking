2b0cf46807c071d583610ffe1e30003eb5debb81
Adanna <aaddanna@outlook.com>
2022-07-26 05:35:33 +0000

    made ensemble model work

--- test1.py
+++ test1.py
@@ -21,34 +21,39 @@ def CIFAR100():
 class MiniModel(nn.Module):
   def __init__(self, init_weights : bool = True):
     super(MiniModel, self).__init__()
-    self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3)
-    self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
-    self.conv2_drop = nn.Dropout2d()
-    self.fc1 = nn.Linear(720, 1024)
-    self.fc2 = nn.Linear(1024, 2)
+    self.features = nn.Sequential(
+      nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3)), 
+      nn.BatchNorm2d(64),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3)), 
+      nn.BatchNorm2d(128),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=(2,2),stride=2),
+    )
+    
+    self.classifier = nn.Linear(25088, 2)
 
     if init_weights:
-      nn.init.kaiming_normal_(self.conv1.weight, mode="fan_out", nonlinearity="relu")
-      nn.init.constant_(self.conv1.weight, 0)
-      nn.init.kaiming_normal_(self.conv2.weight, mode="fan_out", nonlinearity="relu")
-      nn.init.constant_(self.conv2.weight, 0)
-      nn.init.normal_(self.fc1.weight, 0, 0.01)
-      nn.init.constant_(self.fc1.bias, 0)
-      nn.init.normal_(self.fc2.weight, 0, 0.01)
-      nn.init.constant_(self.fc2.bias, 0)
+      for m in self.modules():
+        if isinstance(m, nn.Conv2d):
+          nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+          if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+        if isinstance(m, nn.BatchNorm2d):
+          nn.init.constant_(m.weight, 1)
+          nn.init.constant_(m.bias, 0)
+        if isinstance(m, nn.Linear):
+          nn.init.normal_(m.weight, 0, 0.01)
+          nn.init.constant_(m.bias, 0)
     
   def forward(self, x):
-    x = F.relu(F.max_pool2d(self.conv1(x), 2))
-    x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-    x = x.view(x.shape[0],-1)
-    x = F.relu(self.fc1(x))
-    x = F.dropout(x, training=self.training)
-    x = self.fc2(x)
-    return x
+    x = self.features(x)
+    x = x.view(x.size(0), -1)
+    return self.classifier(x)
 
 
 class EnsembleBasedModel(nn.Module):
-    def __init__(self, dataset=CIFAR100(), num_classes=100,batch_size=32,epochs=2):
+    def __init__(self, dataset=CIFAR100(), num_classes=100,batch_size=32,epochs=5):
       super(EnsembleBasedModel, self).__init__()
       self.dataset, self.test_dataset = dataset
       self.test_loader = DataLoader(self.test_dataset, batch_size=batch_size, shuffle=True)
@@ -74,7 +79,7 @@ class EnsembleBasedModel(nn.Module):
     def models_forward(self, x):
       outputs = []
       for model in self.models_and_data:
-        outputs.append(self.sop(model(x)))
+        outputs.append(self.sop(model(x))) 
       output_tensor = torch.tensor(outputs)
       if output_tensor.size(0) > 1:
         x, y = output_tensor.shape
