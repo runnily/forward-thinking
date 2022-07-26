e04cacb05b777c13fbf3ae7b44fef815dca0ee8a
Adds <32201807+runnily@users.noreply.github.com>
2022-07-26 04:18:03 +0100

    Added variables

--- test1.py
+++ test1.py
@@ -1,7 +1,5 @@
-from scipy.sparse.extract import tril
 import torch.nn as nn
 import torch
-from sklearn.linear_model import OrthogonalMatchingPursuit
 from torchvision.transforms import ToTensor, Normalize, Compose, Resize
 import torchvision
 import numpy as np
@@ -21,22 +19,50 @@ def CIFAR100():
   return train_data, test_data
 
 class MiniModel(nn.Module):
-  pass
+  def __init__(self, init_weights : bool = True):
+    super(MiniModel, self).__init__()
+    self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3)
+    self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
+    self.conv2_drop = nn.Dropout2d()
+    self.fc1 = nn.Linear(720, 1024)
+    self.fc2 = nn.Linear(1024, 2)
+
+    if init_weights:
+      nn.init.kaiming_normal_(self.conv1.weight, mode="fan_out", nonlinearity="relu")
+      nn.init.constant_(self.conv1.weight, 0)
+      nn.init.kaiming_normal_(self.conv2.weight, mode="fan_out", nonlinearity="relu")
+      nn.init.constant_(self.conv2.weight, 0)
+      nn.init.normal_(self.fc1.weight, 0, 0.01)
+      nn.init.constant_(self.fc1.bias, 0)
+      nn.init.normal_(self.fc2.weight, 0, 0.01)
+      nn.init.constant_(self.fc2.bias, 0)
+    
+  def forward(self, x):
+    x = F.relu(F.max_pool2d(self.conv1(x), 2))
+    x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+    x = x.view(x.shape[0],-1)
+    x = F.relu(self.fc1(x))
+    x = F.dropout(x, training=self.training)
+    x = self.fc2(x)
+    return x
+
 
 class EnsembleBasedModel(nn.Module):
     def __init__(self, dataset=CIFAR100(), num_class=100,batch_size=32,epochs=2):
+      super(EnsembleBasedModel, self).__init__()
       self.dataset, self.test_dataset = dataset
+      self.test_loader = DataLoader(self.test_dataset, batch_size=batch_size,num_workers=2, shuffle=True)
       self.num_classes = 100
-      self.createModels(batch_size)
       self.models_and_data = {}
+      self.createModels(batch_size)
       self.epochs = epochs
 
     def forward(self, x):
       return self.models_forward(x)
 
     def sop(self, y):
-      softmax_out = F.softmax(y)
-      if softmax_out.shape(0) > 1:
+      softmax_out = F.softmax(y, 1)
+      if softmax_out.size(0) > 1:
         return [self.maximum(x) for x in softmax_out]
       return self.maximum(softmax_out)
 
@@ -50,7 +76,7 @@ class EnsembleBasedModel(nn.Module):
       for model in self.models_and_data:
         outputs.append(self.sop(model(x)))
       output_tensor = torch.tensor(outputs)
-      if output_tensor.shape(0) > 1:
+      if output_tensor.size(0) > 1:
         x, y = output_tensor.shape
         return output_tensor.reshape(y,x)
       return output_tensor
@@ -58,7 +84,7 @@ class EnsembleBasedModel(nn.Module):
     def _getSelectedIndicies(self, target):
       selected_target_idx = (torch.tensor(self.dataset.targets) == target).nonzero().reshape(-1)
       selected_target_idx_ops = (torch.tensor(self.dataset.targets) != target).nonzero().reshape(-1)[torch.randperm(500)]
-      return torch.cat(selected_target_idx , selected_target_idx_ops)
+      return torch.cat((selected_target_idx , selected_target_idx_ops))
 
     def createModels(self, batch_size):
       """
@@ -67,18 +93,18 @@ class EnsembleBasedModel(nn.Module):
       """
       targets = set(self.dataset.targets)
       for target in targets:
-        selected_target_idx = self._getSelectedIndicies(self, target) # including a random selection
+        selected_target_idx = self._getSelectedIndicies(target) # including a random selection
         data = Subset(deepcopy(self.dataset), selected_target_idx)
         labels = data.dataset.targets
         labels[labels != target] = 0
         labels[labels == target] = 1
         data.dataset.targets = labels
-        model = MiniModel()
+        model = MiniModel().to(DEVICE)
         self.models_and_data[model] = DataLoader(data, batch_size=batch_size, shuffle=True)
 
     def train_models(self):
       for i, model in enumerate(self.models_and_data):
-        batch_data = self.models_and_data[self.models]
+        batch_data = self.models_and_data[model]
         self.train_a_model(model, batch_data) # need to train model with full train_data? not just one
 
     def train_a_model(self, model, batch_data):
@@ -95,15 +121,15 @@ class EnsembleBasedModel(nn.Module):
           optimizer.zero_grad()   
 
           # forward + backward + optimize
-          outputs = model(images) 
+          outputs = model(images).to(DEVICE)
           loss = criterion(outputs, labels)
           loss.backward()
           optimizer.step()
 
         print("Epoch {}, Loss {}".format(epoch, loss))
+      model.eval()
             
     def test_model(self):
-      self.eval()
       with torch.no_grad():
         n_correct = 0
         n_samples = 0
@@ -123,5 +149,5 @@ class EnsembleBasedModel(nn.Module):
       return accuracy
 
 if __name__ == "__main__":
-  ensemble = EnsembleBasedModel()
+  ensemble = EnsembleBasedModel().to(DEVICE)
   ensemble.test_model()
