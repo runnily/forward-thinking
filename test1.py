691c9797713c3bd05ab87f5b6d620c07f51f56d6
Adanna <aaddanna@outlook.com>
2022-07-27 14:20:49 +0000

    debugging ensemble model

--- test1.py
+++ test1.py
@@ -3,18 +3,20 @@ import torch
 from torchvision.transforms import ToTensor, Normalize, Compose, Resize
 import torchvision
 import numpy as np
-from torch.utils.data import DataLoader, Subset, dataloader, Dataset
+from torch.utils.data import DataLoader, Subset, dataloader, Dataset # remove subset later
 import torch.optim as optim
 import torch.nn.functional as F
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-transform = Compose(
-    [ToTensor(),
-     Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-test = torchvision.datasets.CIFAR10("./data", train=True, download=True, transform=transform)
-sub_test = Subset(test, (torch.tensor(test.targets) == 1).nonzero().reshape(-1))
-sub_dataloader = DataLoader(sub_test, batch_size=64, shuffle=True)
 
+def debugging():
+  transform = Compose(
+      [ToTensor(),
+      Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+  test = torchvision.datasets.CIFAR10("./data", train=True, download=True, transform=transform)
+  sub_test = Subset(test, (torch.tensor(test.targets) == 2).nonzero().reshape(-1))
+  return DataLoader(sub_test, batch_size=64, shuffle=True)
+  
 class BinarySubsetDataset(Dataset):
   def __init__(self, dataset, indices, target):
     self.dataset = dataset
@@ -28,9 +30,10 @@ class BinarySubsetDataset(Dataset):
   
   def __expand(self, item):
     if item[1] != self.target:
-      return (item[0], 0)
+      # return input, outputs
+      return (item[0].float(), 0)
     else:
-      return (item[0], 1)
+      return (item[0].float(), 1)
     
   def __len__(self):
     return len(self.indices)
@@ -44,7 +47,7 @@ def CIFAR100():
   return train_data, test_data
 
 class MiniBinaryModel(nn.Module):
-  def __init__(self, init_weights : bool = True, num_classes : int = 2):
+  def __init__(self, init_weights : bool = True, num_classes : int = 1):
     super(MiniBinaryModel, self).__init__()
     self.features = nn.Sequential(
       nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3)), 
@@ -55,8 +58,9 @@ class MiniBinaryModel(nn.Module):
       nn.ReLU(inplace=True),
       nn.MaxPool2d(kernel_size=(2,2),stride=2),
     )
+    self.num_classes = num_classes
     
-    self.classifier = nn.Linear(25088, 2)
+    self.classifier = nn.Linear(25088, num_classes)
 
     if init_weights:
       for m in self.modules():
@@ -98,6 +102,9 @@ class EnsembleBasedModel(nn.Module):
       return self.maximum(softmax_out)
 
     def maximum(self,y):
+      """if y > 0.65:
+        return y
+      return 0"""
       if y[1] >= y[0]:
         return y[1]
       return 0
@@ -107,14 +114,17 @@ class EnsembleBasedModel(nn.Module):
       for model in self.models_and_data:
         outputs.append(self.configOutput(model(x))) 
       output_tensor = torch.tensor(outputs)
+      print("here")
+      print(output_tensor)
+      print(output_tensor.shape)
       if output_tensor.size(0) > 1:
         x, y = output_tensor.shape
         return output_tensor.reshape(y,x)
       return output_tensor.to(DEVICE)
 
-    def _getSelectedIndicies(self, target):
-      selected_target_idx = (torch.tensor(self.dataset.targets) == target).nonzero().reshape(-1)
-      selected_target_idx_ops = (torch.tensor(self.dataset.targets) != 5).nonzero().reshape(-1)[torch.randperm(len(selected_target_idx))]
+    def _getSelectedIndicies(self, dataset, target):
+      selected_target_idx = (torch.tensor(dataset.targets) == target).nonzero().reshape(-1)
+      selected_target_idx_ops = (torch.tensor(dataset.targets) != 5).nonzero().reshape(-1)[torch.randperm(len(selected_target_idx))]
       return torch.cat((selected_target_idx,selected_target_idx_ops ))
 
     def createModels(self, batch_size):
@@ -123,15 +133,26 @@ class EnsembleBasedModel(nn.Module):
           Will create a model for every targets
       """
       for _, target in self.dataset.class_to_idx.items(): # should be the same order of test
-        data = BinarySubsetDataset(self.dataset, self._getSelectedIndicies(target), target)
-        model = MiniBinaryModel(num_classes=2).to(DEVICE)
-        self.models_and_data[model] = DataLoader(data, batch_size=batch_size, shuffle=True)
+        train_data = BinarySubsetDataset(self.dataset, self._getSelectedIndicies(self.dataset, target), target)
+        test_data = BinarySubsetDataset(self.test_dataset, self._getSelectedIndicies(self.test_dataset, target), target)
+        model = MiniBinaryModel(num_classes=1).to(DEVICE)
+        self.models_and_data[model] = (
+          DataLoader(
+            train_data, 
+            batch_size=batch_size, 
+            shuffle=True), 
+          DataLoader(
+            test_data, 
+            batch_size=batch_size, 
+            shuffle=True)
+          )
 
     def train_model(self):
       #for epochs in range(self.epochs):
       for i, model in enumerate(self.models_and_data):
-        batch_data = self.models_and_data[model]
-        self._training_model.train(model, batch_data, epochs=1) 
+        data = self.models_and_data[model]
+        self._training_model.train(model, data[1], epochs=5)
+         
       #self.train_loader
 
     def test_model(self):
@@ -147,28 +168,32 @@ class TrainAndTest():
     model, train_loader, epochs = ( model, train_loader, epochs) if (model and train_loader and epochs) else (self.model, self.loader, self.epochs)
     n_total_steps = len(train_loader)
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    criterion = nn.CrossEntropyLoss().to(DEVICE)
+    criterion = None
+    if model.num_classes > 1:
+      criterion = nn.CrossEntropyLoss().to(DEVICE)
+    else:
+      criterion = nn.BCELoss().to(DEVICE)
     for epoch in range(epochs): 
       model.train()
       for i, (images, labels) in enumerate(
         train_loader, 0):
         images = images.to(DEVICE)
-        labels = labels.to(DEVICE)
+        labels = labels.to(DEVICE) if (model.num_classes > 1) else labels.unsqueeze(1).to(DEVICE).to(torch.float32)
         optimizer.zero_grad()  
-        outputs = model(images)
+        outputs = model(images) if (model.num_classes > 1) else torch.sigmoid(model(images))
         loss = criterion(outputs, labels)
         loss.backward()  
         optimizer.step()  
-      """   if (i + 1) % 100 == 0:
+        if (i + 1) % 100 == 0:
           print(
             "Epoch [{}/{}], Step [{}/{}], Loss: {:.2f}".format(
             epoch + 1, epochs, i + 1, n_total_steps, loss.item()
             )
-          )    
-      print("Test accuracy {}, at epoch: {}".format(self.test(model, train_loader), epoch))"""
+        )    
+      print("Test accuracy {}, at epoch: {}".format(self.test(model, train_loader), epoch))
 
   def test(self, model=None, test_loader=None):
-    model, test_loader = (model, test_loader) if (model and test_loader) else (self.model, sub_dataloader)
+    model, test_loader = (model, test_loader) if (model and test_loader) else (self.model, debugging())
     model.eval()
     with torch.no_grad():
       n_correct = 0
@@ -178,9 +203,12 @@ class TrainAndTest():
         labels = labels.to(DEVICE)
         outputs = model(images)
         #print("here")
-        print(labels) 
+        #print(labels) 
         _, predicted = torch.max(outputs.data, 1)
-        print(predicted)
+        if model == self.model:
+          print(labels)
+          print(predicted)
+        #print(predicted)
         n_samples += labels.size(0) 
         n_correct += (
             (predicted.to(DEVICE) == labels).sum().item()
@@ -193,4 +221,4 @@ class TrainAndTest():
 if __name__ == "__main__":
   ensemble = EnsembleBasedModel().to(DEVICE)
   ensemble.train_model()
-  print(ensemble.test_model())
+  #print(ensemble.test_model())
